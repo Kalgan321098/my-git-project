3082cae78d4ca03fa4610c8072ba0e96c2503b24
Kalgan321098 <bandi.pro321098@gmail.com>
2026-04-30 19:44:15 +0300

    Kalkylyator

---
+++ calculator.py
@@ -0,0 +1,17 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    return 'Ошибка: деление на ноль'
+
+print('Калькулятор загружен!')
+print(f'2 + 3 = {add(2, 3)}')
