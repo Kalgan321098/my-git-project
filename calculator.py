a34c3baecf7c352bebfe1f353d374a9b184588d6
Kalgan321098 <bandi.pro321098@gmail.com>
2026-05-01 17:34:35 +0300

    Добавил govna

--- calculator.py
+++ calculator.py
@@ -1,4 +1,3 @@
-
 def add(a, b):
     return a + b
 
@@ -15,4 +14,28 @@ def divide(a, b):
 
 print('Калькулятор загружен!')
 print(f'2 + 3 = {add(2, 3)}')
-print(f'5 - 2 = {subtract(5, 2)}')
+
+if __name__ == '__main__':
+    while True:
+        try:
+            a = float(input('Введите первое число (или 0 для выхода): '))
+            if a == 0:
+                print('Выход из калькулятора.')
+                break
+            b = float(input('Введите второе число: '))
+            operation = input('Введите операцию (+, -, *, /): ')
+
+            if operation == '+':
+                print(f'{a} + {b} = {add(a, b)}')
+            elif operation == '-':
+                print(f'{a} - {b} = {subtract(a, b)}')
+            elif operation == '*':
+                print(f'{a} * {b} = {multiply(a, b)}')
+            elif operation == '/':
+                print(f'{a} / {b} = {divide(a, b)}')
+            else:
+                print('Неизвестная операция. Используйте +, -, * или /.')
+        except ValueError:
+            print('Ошибка: введите корректное число.')
+        except Exception as e:
+            print(f'Произошла непредвиденная ошибка: {e}')
